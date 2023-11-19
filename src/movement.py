7551a8eee4b31c69b3dc041ddbe3e9d8197581b2
Kinantan Bagaspati <kinantanyolo@gmail.com>
2023-11-19 18:59:03 +0700

    init dfs

--- src/movement.py
+++ src/movement.py
@@ -11,24 +11,69 @@ def movement(myPlayer):
     return move
 
 def select_movement(myPlayer):
-    '''
-    Ubahlah algoritma perpindahan yang dipilih player dengan mengubah fungsi ini.
-    Untuk saat ini fungsi masih mengembalikan random
-    Keluaran dari fungsi ini adalah arah gerakan player:
-        "W" --> player akan berpindah ke atas
-        "A" --> player akan berpindah ke kiri
-        "S" --> player akan berpindah ke bawah
-        "D" --> player akan berpindah ke kanan
-    Silahkan tambahkan atribut/ method di kelas Player jika dibutuhkan, tapi jangan menghapus/ mengganti atribut/ kelas yang sudah ada.
-    '''
-    '''
-    Change the player's selected movement algorithm by changing this function.
-    For now, the function still returns random.
-    The output of this function is the player's movement direction:
-        "W" --> player will move up
-        "A" --> player will move to the left
-        "S" --> player will move down
-        "D" --> player will move to the right
-    You can add attributes and/or methods in class Player if needed, but do not delete or replace existing attributes and classes.
-    '''
-    return random.choice(["W", "A", "S", "D"])
+    if len(myPlayer.backtrack_stack) > 0 and myPlayer.backtrack_stack[-1] == (myPlayer.x, myPlayer.y):
+        if myPlayer.prev_move == "S": # menabrak tembok bawah, sekarang player tahu jml baris board
+            myPlayer.board_nbrows = myPlayer.x + 1
+            myPlayer.memory.pop((myPlayer.x + 1, myPlayer.y), "")
+        elif myPlayer.prev_move == "D": # menabrak tembok kanan, sekarang player tahu jml kolom board
+            myPlayer.board_nbcols = myPlayer.y + 1
+            myPlayer.memory.pop((myPlayer.x, myPlayer.y + 1), "")
+        myPlayer.backtrack_stack.pop()
+    else: # player berhasil bergerak pada move sebelumnya
+        to_apply = 0
+        if myPlayer.percept["stench"]:
+            to_apply += 1
+        if myPlayer.percept["breeze"]:
+            to_apply += 2
+
+        updated_coordinates: list[tuple[int, int]] = []
+        if myPlayer.x > 0:
+            updated_coordinates.append((myPlayer.x-1, myPlayer.y))
+        if myPlayer.y > 0:
+            updated_coordinates.append((myPlayer.x, myPlayer.y-1))
+        if myPlayer.board_nbrows < 0 or myPlayer.x < myPlayer.board_nbrows - 1:
+            updated_coordinates.append((myPlayer.x+1, myPlayer.y))
+        if myPlayer.board_nbcols < 0 or myPlayer.y < myPlayer.board_nbcols - 1:
+            updated_coordinates.append((myPlayer.x, myPlayer.y+1))
+
+        for pos in updated_coordinates:
+            if not(pos in myPlayer.memory):
+                myPlayer.memory[pos] = 3
+            # print(pos, ":", myPlayer.memory[pos])
+            add_to_stack = (myPlayer.memory[pos] > 0)
+            myPlayer.memory[pos] = (myPlayer.memory[pos] & to_apply)
+            # print(pos, ":", myPlayer.memory[pos])
+            add_to_stack = add_to_stack and (myPlayer.memory[pos] == 0)
+            if add_to_stack:    
+                myPlayer.safe_unvisited_stack.append(pos)
+    
+    # Memutuskan move
+    myPlayer.prev_x = myPlayer.x
+    myPlayer.prev_y = myPlayer.y
+    possible_moves = {
+        (myPlayer.x-1, myPlayer.y): "W",
+        (myPlayer.x, myPlayer.y-1): "A",
+        (myPlayer.x+1, myPlayer.y): "S",
+        (myPlayer.x, myPlayer.y+1): "D",
+    }
+
+    while len(myPlayer.safe_unvisited_stack) > 0 and (
+        myPlayer.safe_unvisited_stack[-1][0] == myPlayer.board_nbrows or myPlayer.safe_unvisited_stack[-1][1] == myPlayer.board_nbcols
+    ):
+        myPlayer.safe_unvisited_stack.pop()
+    # print("myPlayer.safe_unvisited_stack", myPlayer.safe_unvisited_stack)
+    if len(myPlayer.safe_unvisited_stack) == 0:
+        # Player tidak punya cell yang 100% aman, kemungkinan kecil
+        # Hanya terjadi saat wumpus dan pit mengelilingi posisi awal pemain, atau posisi akhir gold
+        myPlayer.prev_move = random.choice(["W", "A", "S", "D"])
+        myPlayer.backtrack_stack.append((myPlayer.x, myPlayer.y))
+    elif myPlayer.safe_unvisited_stack[-1] in possible_moves: 
+        # Jalan ke cell aman yang belum terkunjungi
+        myPlayer.prev_move = possible_moves[myPlayer.safe_unvisited_stack[-1]]
+        myPlayer.safe_unvisited_stack.pop()
+        myPlayer.backtrack_stack.append((myPlayer.x, myPlayer.y))
+    else: # Backtrack
+        myPlayer.prev_move = possible_moves[myPlayer.backtrack_stack[-1]]
+        myPlayer.backtrack_stack.pop()
+
+    return myPlayer.prev_move
--- src/player.py
+++ src/player.py
@@ -10,6 +10,24 @@ class Player(object):
             "breeze": False,
         }
 
+        # Atribut baru
+
+        # Untuk board size, player dapat mengetahui dari apakah move dia sebelumnya menempatkan pada posisi yang sama atau tidak
+        # Setelah tau informasi baru ini, player menempatkan pada board_nbrows atau board_nbcols
+        self.prev_move: str = ""
+        self.board_nbrows: int = -1
+        self.board_nbcols: int = -1
+
+        # Untuk keperluan DFS dan memori player akan potensi cell
+        self.memory: dict[tuple[int, int], int] = {(x, y): 0}
+        # 0 untuk safe cell
+        # 1 untuk safe atau Wumpus cell
+        # 2 untuk safe atay Pit cell
+        # 3 untuk bisa safe, Wumpus, atau Pit cell
+        # Perhatikan bahwa operasi bitwise or dapat dimanfaatkan disini
+        self.backtrack_stack: list[tuple[int, int]] = []
+        self.safe_unvisited_stack: list[tuple[int, int]] = []
+
     def move(self, board, direction):
         board.update_board(self.x, self.y, board.board_static[self.x][self.y])
 
@@ -40,3 +58,4 @@ class Player(object):
             print("======================\nCongratulations, you win 😄")
             return True
         return False
+    
